83550c0312ff45de520d16ac0f657e20536f5da7
Alex le Jeune <a.lejeune@live.be>
2025-07-07 15:39:35 +0200

    Remove some files from git

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var waterfallStartD2BF53E768064D19AD9FB499E5BCD67A_DEBUG: IVisualPlugin;
-export default waterfallStartD2BF53E768064D19AD9FB499E5BCD67A_DEBUG;
